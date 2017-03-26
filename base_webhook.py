4c2dfb8774f12e40191eec53bd11f68878c57ef6
Cristian Cortes <cacortes1@uc.cl>
2017-03-25 23:48:24 -0300

    webhook_base

---
+++ base_webhook.py
@@ -0,0 +1,32 @@
+import flask
+import telepot
+from os import environ
+try:
+	from Queue import Queue
+except ImportError:
+	from queue import Queue
+
+
+BOT_TOKEN = '318756416:AAHSgDPf-XJWUuImHoEKoJqvWAZf2TSqQgU'
+TOKEN = environ(BOT_TOKEN)
+app = flask(__name__)
+SECRET = "/bot{}".format(TOKEN)
+url = "https://deptoon.herokuapp.com/"
+BOT = telepot.Bot(TOKEN)
+UPDATE_QUEUE = Queue()
+
+
+def on_chat_message(msg):
+	content_type, chat_type, chat_id = telepot.glance(msg)
+	BOT.sendMessage(chat_id, 'hello!')
+
+
+BOT.message_loop({'chat': on_chat_message}, source=UPDATE_QUEUE)
+
+@app.route(SECRET, methods=['GET', 'POST'])
+def pass_update():
+	UPDATE_QUEUE.put(request.data)
+	return 'OK'
+
+
+BOT.setWebhook()
