1cf6aed8f375c2f3f9acf45c62c9212ba529eff2
Cristian Cortes <cacortes1@uc.cl>
2017-03-31 00:09:17 -0300

    new change for heroku

--- base_webhook.py
+++ base_webhook.py
@@ -1,58 +1,4 @@
-from flask import Flask, request
 import telepot
-from telepot.delegate import per_chat_id, create_open, pave_event_space
-from time import sleep
-try:
-    from Queue import Queue
-except ImportError:
-    from queue import Queue
-import os
-ON_HEROKU = os.environ.get('ON_HEROKU')
-
-if ON_HEROKU:
-    # get the heroku port
-    PORT = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    PORT = 3000
-
-"""
-$ python3.5 flask_counter.py <token> <listening_port> <webhook_url>
-Webhook path is '/abc', therefore:
-<webhook_url>: https://<base>/abc
-"""
-
-class MessageCounter(telepot.helper.ChatHandler):
-    def __init__(self, *args, **kwargs):
-        super(MessageCounter, self).__init__(*args, **kwargs)
-        self._count = 0
-
-    def on_chat_message(self, msg):
-        self._count += 1
-        self.sender.sendMessage(self._count)
-
-TOKEN = "318756416:AAHSgDPf-XJWUuImHoEKoJqvWAZf2TSqQgU"
-# HTOKEN = os.environ(TOKEN)
-SECRET = "bot{}".format(TOKEN)
-URL = "https://api.telegram.org/"
-app = Flask(__name__)
-update_queue = Queue()  # channel between `app` and `bot`
-
-bot = telepot.DelegatorBot(TOKEN, [
-    pave_event_space()(
-        per_chat_id(), create_open, MessageCounter, timeout=10),
-])
-bot.message_loop(source=update_queue)  # take updates from queue
-
-@app.route('/abc', methods=['GET', 'POST'])
-def pass_update():
-    update_queue.put(request.data)  # pass update to bot
-    return 'OK'
-
-if __name__ == '__main__':
-    bot.setWebhook(URL+SECRET)
-    sleep(1)
-    app.run(port=PORT, debug=True)
-"""import telepot
 from telepot.delegate import per_chat_id, create_open, pave_event_space
 from flask import Flask, request
 # import os
@@ -61,14 +7,21 @@ try:
     from Queue import Queue
 except ImportError:
     from queue import Queue
+"""ON_HEROKU = os.environ.get('ON_HEROKU')
 
+if ON_HEROKU:
+    # get the heroku port
+    PORT = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    PORT = 3000
+"""
 
 TOKEN = '318756416:AAHSgDPf-XJWUuImHoEKoJqvWAZf2TSqQgU'
 # HTOKEN = os.environ(TOKEN)
 app = Flask(__name__)
 SECRET = "/bot{}".format(TOKEN)
 URL = "https://api.telegram.org/"
-# BOT = telepot.Bot(TOKEN)
+BOT = telepot.Bot(TOKEN)
 UPDATE_QUEUE = Queue()
 dawg_list = cycle([
                     'Dawg acuerdate de comprar las tazas',
@@ -90,29 +43,13 @@ def on_chat_message(msg):
                     answer = "Yow yow"
     BOT.sendMessage(chat_id, "{}".format(answer))
 
-
 BOT.message_loop({'chat': on_chat_message}, source=UPDATE_QUEUE)
-class MessageCounter(telepot.helper.ChatHandler):
-    def __init__(self, *args, **kwargs):
-        super(MessageCounter, self).__init__(*args, **kwargs)
-        self._count = 0
 
-    def on_chat_message(self, msg):
-        self._count += 1
-        self.sender.sendMessage(self._count)
 
 @app.route(SECRET, methods=['GET', 'POST'])
 def pass_update():
     UPDATE_QUEUE.put(request.data)
     return 'OK'
 
-
-bot = telepot.DelegatorBot(TOKEN, [
-    pave_event_space()(
-        per_chat_id(), create_open, MessageCounter, timeout=10),
-])
-bot.message_loop(source=UPDATE_QUEUE)
-
-
 bot.setWebhook(URL + SECRET)
-"""
+app.run(port=8080, debug=True)
