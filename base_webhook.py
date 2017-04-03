cb8d81ebfaf1e7c04f66215efec7d0be26ed64cc
Cristian Cortes <cacortes1@uc.cl>
2017-04-03 16:52:20 -0300

    fix delete

--- base_webhook.py
+++ base_webhook.py
@@ -33,30 +33,35 @@ class MessageCounter(telepot.helper.ChatHandler):
         text = msg['text']
         if text.startswith('/start'):
             bot.sendMessage(chat_id, 'Deptoon_bot is ready to begin')
+
         elif text.lower().startswith('/chaqueteardawg'):
             bot.sendMessage(chat_id, choice(dawg_list))
             # self.sender.sendMessage(self._count) no sirve para grupos
+
         elif text.startswith("/addchaqueteo"):
             text = text.replace("/addchaqueteo", "").lstrip()
             dawg_list.append(text)
             answer = "'{}' fue agregado al chaqueteo del dawg".format(text)
             bot.sendMessage(chat_id, answer)
+
         elif text.startswith("/listadawg"):
             answer = "** Chaqueteando al Dawg **\n\n"
             for i in range(len(dawg_list)):
                 answer += "{}.- {}\n".format(i+1, dawg_list[i])
             bot.sendMessage(chat_id, answer)
+
         elif text.startswith("/deletechaqueteo"):
             text = text.replace("/deletechaqueteo").lstrip()
             try:
                 answer = "'{}' fue eliminado de la lista de chaqueteo del dawg".format(dawg_list[text])
-                del dawg_list[int(text)]
+                del dawg_list[int(text) - 1]
             except ValueError:
                 answer = "El valor enviado no es un indice valido, prueba llamando a /listadawg para obtener el valor que buscas"
             except IndexError:
                 answer = "El valor enviado no pertenece a la lista de chaqueteo del dawg, prueba llamando a /listadawg para obtener el valor que buscas"
             finally:
                 bot.sendMessage(chat_id, answer)
+
         else:
             pass
 
