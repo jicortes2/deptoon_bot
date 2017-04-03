3dc8c52b735455e5d02ada00a6f7e4d52fe523a4
Cristian Cortes <cacortes1@uc.cl>
2017-04-02 22:11:52 -0300

    restart bot

--- base_webhook.py
+++ base_webhook.py
@@ -1,48 +1,47 @@
-import telepot
+import sys
 from flask import Flask, request
-# import os
-from itertools import cycle
+import telepot
+from telepot.delegate import per_chat_id, create_open, pave_event_space
+
 try:
     from Queue import Queue
 except ImportError:
     from queue import Queue
 
+"""
+$ python3.5 flask_counter.py <token> <listening_port> <webhook_url>
+Webhook path is '/abc', therefore:
+<webhook_url>: https://<base>/abc
+"""
 
-TOKEN = '318756416:AAHSgDPf-XJWUuImHoEKoJqvWAZf2TSqQgU'
-# HTOKEN = os.environ(TOKEN)
+class MessageCounter(telepot.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(MessageCounter, self).__init__(*args, **kwargs)
+        self._count = 0
+
+    def on_chat_message(self, msg):
+        self._count += 1
+        self.sender.sendMessage(self._count)
+
+
+TOKEN = '361066388:AAH-TSjo2oz1XzDMCcRz_bRfW4KHej-M3so'
+PORT = 8000
+URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
+
 app = Flask(__name__)
-SECRET = "/bot{}".format(TOKEN)
-URL = "https://stark-tor-45686.herokuapp.com/"
-BOT = telepot.Bot(TOKEN)
-UPDATE_QUEUE = Queue()
-dawg_list = cycle([
-                    'Dawg acuerdate de comprar las tazas',
-                    'Dawg, no te ibai en marzo?',
-                    'Hace cuanto no vas al supermercado dawg?',
-                    'Te acuerdas donde queda el super dawg?',
-                    'Dawg compra pan'
-                ])
+update_queue = Queue()  # channel between `app` and `bot`
 
+bot = telepot.DelegatorBot(TOKEN, [
+    pave_event_space()(
+        per_chat_id(), create_open, MessageCounter, timeout=10),
+])
+bot.message_loop(source=update_queue)  # take updates from queue
 
-def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type != 'text':
-        return
-    text = msg["text"]
-    print("entra")
-    if text.lower().startswith('/chaqueteardawg'):
-        answer = dawg_list
-    else:
-        answer = "Yow yow"
-    BOT.sendMessage(chat_id, "{}".format(answer))
-
-BOT.message_loop({'chat': on_chat_message}, source=UPDATE_QUEUE)
-
-
-@app.route(SECRET, methods=['GET', 'POST'])
+@app.route('/abc', methods=['GET', 'POST'])
 def pass_update():
-    UPDATE_QUEUE.put(request.data)
+    update_queue.put(request.data)  # pass update to bot
     return 'OK'
 
-BOT.setWebhook(URL + SECRET)
-app.run(host='0.0.0.0', port=8080, debug=False)
+if __name__ == '__main__':
+    bot.setWebhook(URL)
+    app.run(port=PORT, debug=True)
