9fe4fa9fdd82b593f862eae2328eff0b1c813337
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-04-04 13:05:18 -0300

    methods for each command

--- deptoon_bot.py
+++ deptoon_bot.py
@@ -12,6 +12,81 @@ class Deptoon(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(Deptoon, self).__init__(*args, **kwargs)
 
+    def new_phrase(self, command):
+        """ Agrega una frase para chaquetear al dawg """
+        command = command.replace("/addchaqueteo", "").lstrip()
+        with open("db/dawg_list.txt", "a") as file:
+            file.write("{}\n".format(command))
+        return "'{}' fue agregado al chaqueteo del dawg".format(command)
+
+    def get_phrases(self):
+        """ Listado de frases para molestar al dawg """
+        phrases = "** Chaqueteando al Dawg **\n\n"
+        with open("db/dawg_list.txt") as file:
+            dawg_list = file.readlines()
+            for i, phrase in enumerate(dawg_list):
+                phrases += "{}.- {}".format(i+1, phrase)
+        return phrases
+
+    def get_phrase(self):
+        """ Retorna una frase para molestar al dawg """
+        with open("db/dawg_list.txt", "r") as f:
+            datos = f.readlines()
+        return choice(datos)
+
+    def delete_phrase(self, command):
+        """ Elimina una frase del listado para chaquetear al dawg """
+        try:
+            with open("db/dawg_list.txt") as file:
+                facts = file.readlines()
+
+            text = int(command.replace("/deletechaqueteo", "").lstrip())
+            if int(text) < 0:
+                answer = "'{}' fue eliminado de la lista de chaqueteo del dawg".format(
+                    facts[text])
+                del facts[text]
+            else:
+                answer = "'{}' fue eliminado de la lista de chaqueteo del dawg".format(
+                    facts[text - 1])
+                del facts[text - 1]
+            with open("db/dawg_list.txt", "w") as file:
+                for dato in facts:
+                    file.write("{}".format(dato))
+            return answer
+
+        except ValueError:
+            answer = "El valor enviado no es un indice valido, prueba llamando a /listadawg para obtener el valor que buscas"
+        except IndexError:
+            answer = "El valor enviado no pertenece a la lista de chaqueteo del dawg, prueba llamando a /listadawg para obtener el valor que buscas"
+
+    def add_products(self, command):
+        """ Agrega productos al carrito de supermercado """
+        products = command.replace("/add", "").lstrip().split(',')
+        if len(products) == 0:
+            return "Debes ingresar los productos asi: /add prod1, prod2, ..."
+        else:
+            with open("db/shop.txt", 'a') as f:
+                for prod in products:
+                    f.write("{}\n".format(prod.lstrip()))
+            if len(products) == 1:
+                return "Se agregó 1 producto al carrito"
+            else:
+                return "Se agregaron {} productos al carrito".format(len(products))
+
+    def clear_supermarket(self):
+        """ Vacia el carrito de supermercado """
+        with open("db/shop.txt", 'w'):
+            pass
+        return "Gracias por su compra, espero que no hayas olvidado nada!"
+
+    def supermarket_list(self):
+        """ Listado de elementos en el carrito """
+        products = "CARRITO DE SUPERMERCADO\n\n"
+        with open("db/shop.txt", 'r') as f:
+            for prod in f:
+                products += "- {}".format(prod)
+        return products
+
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
 
@@ -24,67 +99,25 @@ class Deptoon(telepot.helper.ChatHandler):
             answer = "Yow yow aqui deptoon_bot listo para zorronear"
 
         elif text.lower().startswith('/chaqueteardawg'):
-            # Retorna una frase para molestar al dawg
-            with open("db/dawg_list.txt") as file:
-                datos = file.readlines()
-            answer = choice(datos)
+            answer = self.get_phrase()
 
         elif text.startswith("/addchaqueteo"):
-            # Agrega una frase para chaquetear al dawg
-            text = text.replace("/addchaqueteo", "").lstrip()
-            with open("db/dawg_list.txt", "a") as file:
-                file.write("{}\n".format(text))
-            answer = "'{}' fue agregado al chaqueteo del dawg".format(text)
+            answer = self.new_phrase(text)
 
         elif text.startswith("/listadawg"):
-            # Listado de frases para molestar al dawg
-            answer = "** Chaqueteando al Dawg **\n\n"
-            with open("db/dawg_list.txt") as file:
-                dawg_list = file.readlines()
-                for i, phrase in enumerate(dawg_list):
-                    answer += "{}.- {}".format(i+1, phrase)
+            answer = self.get_phrases()
 
         elif text.startswith("/deletechaqueteo"):
-            # Elimina una frase del listado para chaquetear al dawg
-            try:
-                with open("db/dawg_list.txt") as file:
-                    facts = file.readlines()
+            answer = self.delete_phrase(text)
 
-                text = int(text.replace("/deletechaqueteo", "").lstrip())
-                if int(text) < 0:
-                    answer = "'{}' fue eliminado de la lista de chaqueteo del dawg".format(
-                        facts[text])
-                    del facts[text]
-                else:
-                    answer = "'{}' fue eliminado de la lista de chaqueteo del dawg".format(
-                        facts[text - 1])
-                    del facts[text - 1]
-                with open("db/dawg_list.txt", "w") as file:
-                    for dato in facts:
-                        file.write("{}".format(dato))
-
-            except ValueError:
-                answer = "El valor enviado no es un indice valido, prueba llamando a /listadawg para obtener el valor que buscas"
-            except IndexError:
-                answer = "El valor enviado no pertenece a la lista de chaqueteo del dawg, prueba llamando a /listadawg para obtener el valor que buscas"
         elif text.startswith("/add"):
-            # Agrega productos al carrito de supermercado
-            products = text.replace("/add", "").lstrip().split(',')
-            with open("db/shop.txt", 'a') as f:
-                for prod in products:
-                    f.write("{}\n".format(prod))
-            answer = "Se agregaron {} productos al carrito".format(len(products))
+            answer = self.add_products(text)
+
         elif text.startswith("/clear"):
-            # Vacia el carrito de supermercado
-            with open("db/shop.txt", 'w') as f:
-                pass
-            answer = "Gracias por su compra, espero que no hayas olvidado nada!"
+            answer = self.clear_supermarket()
+
         elif text.startswith("/supermercado"):
-            # Listado de elementos en el carrito
-            answer = "CARRITO DE SUPERMERCADO\n\n"
-            with open("db/shop.txt", 'r') as f:
-                for i, prod in enumerate(f):
-                    answer += "{}. {}".format(i, prod)
+            answer = self.supermarket_list()
 
         BOT.sendMessage(chat_id, answer, parse_mode="Markdown")
 
