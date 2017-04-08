87448283bcb87259262c57d7a3b88f93f09656a5
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-04-08 02:02:15 -0300

    constants of db

--- constants.py
+++ constants.py
@@ -5,3 +5,9 @@ TOKEN = "318756416:AAHSgDPf-XJWUuImHoEKoJqvWAZf2TSqQgU"  # Deptoon_bot
 # URL = "https://deptoon.herokuapp.com/{}".format(TOKEN)
 URL = "https://dry-mesa-62011.herokuapp.com/{}".format(TOKEN)
 DATABASE_URL = "postgres://nhbjuqabjbkjna:06221773db7edf8a65b1ecee698a849491f9eefacd6503f0babfc23263139792@ec2-107-20-186-238.compute-1.amazonaws.com:5432/d7sfoedho05i3j"
+
+DB_NAME = 'd7sfoedho05i3j'
+DB_USER = 'nhbjuqabjbkjna'
+DB_PORT = '5432'
+DB_PASS = '06221773db7edf8a65b1ecee698a849491f9eefacd6503f0babfc23263139792'
+DB_HOST = 'ec2-107-20-186-238.compute-1.amazonaws.com'
--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from psycopg2 import connect, IntegrityError
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import os
 import urllib.parse as urlparse
-from constants import DATABASE_URL
+from constants import DB_NAME, DB_HOST, DB_PASS, DB_PORT, DB_USER
 
 """ table1 = dawg_list (chat int, phrase str)
     table2 = shop (chat int, phrase str) """
@@ -11,15 +11,15 @@ from constants import DATABASE_URL
 def access(option=True):
     """ Returns connection to deptoon database (except when you're
     creating it, on that case returns heroku main database) """
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ[DATABASE_URL])
-    db = url.path[1:]
+    # urlparse.uses_netloc.append("postgres")
+    # url = urlparse.urlparse(os.environ[DATABASE_URL])
+    # db = url.path[1:]
     return connect(
-                    dbname=db,
-                    user=url.username,
-                    password=url.password,
-                    host=url.hostname,
-                    port=url.port
+                    dbname=DB_NAME,
+                    user=DB_USER,
+                    password=DB_PASS,
+                    host=DB_HOST,
+                    port=DB_PORT
                 )
 
 
