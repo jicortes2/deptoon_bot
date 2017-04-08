129f3ab1d16fe197c86c7647cef831fd287c4d38
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-04-08 01:35:51 -0300

    db

--- constants.py
+++ constants.py
@@ -4,4 +4,4 @@ TOKEN = "318756416:AAHSgDPf-XJWUuImHoEKoJqvWAZf2TSqQgU"  # Deptoon_bot
 
 # URL = "https://deptoon.herokuapp.com/{}".format(TOKEN)
 URL = "https://dry-mesa-62011.herokuapp.com/{}".format(TOKEN)
-DATABASE_URL = 'postgres://zetxjajdayechr:84f646c83d12d6460e5f5f0f292c901463e8190a65e7d38250532c5e8db35006@ec2-107-21-108-204.compute-1.amazonaws.com:5432/d1hbe36mkmc705'
+DATABASE_URL = 'nhbjuqabjbkjna:06221773db7edf8a65b1ecee698a849491f9eefacd6503f0babfc23263139792@ec2-107-20-186-238.compute-1.amazonaws.com:5432/d7sfoedho05i3j'
